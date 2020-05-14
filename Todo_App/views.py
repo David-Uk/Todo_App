30ecc73fc0c04b7bcf59f1ca08fa20e689abb8cf
David Ukelere <david.ukelere291@gmail.com>
2020-05-14 09:40:52 +0100

    Modified templates

--- Todo_App/views.py
+++ Todo_App/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home(request):
-    return render('index.html')
+    return render(request, 'main/index.html')
+
+
+@csrf_exempt
+def add_todo(request):
+    print(request.POST)
+    return render(request, 'main/index')
